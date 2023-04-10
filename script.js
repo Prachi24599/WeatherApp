7b9164e2013761241978a0801141c68bbb6c5bb0
Prachi24599 <prachi24599@gmail.com>
2023-04-10 23:58:40 +0530

    added style for search container

--- script.js
+++ script.js
@@ -146,7 +146,7 @@ async function fetchSearchWeatherInfo(cityName) {
 
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
     );
     const data = await response.json();
     loadingScreen.classList.remove("active");
